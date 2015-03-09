b16a19199a42b6e40e69a3a44d043b367e47befc
Henrique Carvalho <henrique.o.carvalho@hotmail.com.br>
2015-03-09 14:55:43 -0700

    Update Song Model
    syntax error

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,7 @@
 class Song < ActiveRecord::Base
-  has_many: like
-  has_many: comment
-  validates: title, presence: true
-  validates: embed_code, presence:true
+  has_many :likes
+  has_many :comments
+
+  validates :title, presence: true
+  validates :embed_code, presence:true
 end
