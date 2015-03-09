085205e28fc35cbd891630079bc3b565874bb691
Henrique Carvalho <henrique.o.carvalho@hotmail.com.br>
2015-03-09 14:27:41 -0700

    Added Song, Like, and Comment Models

---
+++ app/models/comment.rb
@@ -0,0 +1,5 @@
+class Comment < ActiveRecord::Base
+  belongs_to :song
+  validates: song, presence: true
+  validates: body, presence: true
+end
---
+++ app/models/song.rb
@@ -0,0 +1,6 @@
+class Song < ActiveRecord::Base
+  has_many: like
+  has_many: comment
+  validates: title, presence: true
+  validates: embed_code, presence:true
+end
